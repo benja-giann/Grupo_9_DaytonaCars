e5d4214e465143b94e81bb8546a4a010c6067c89
Ivan-Cure-Chegwin <ivancurech@gmail.com>
2022-03-21 19:15:15 -0500

    agregando paginas de registro y detalles del producto

--- src/app.js
+++ src/app.js
@@ -8,5 +8,10 @@ app.use(express.static(path.join(__dirname,"../", "public")));
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")))
 
+app.get("/carrito", (req, res) => res.sendFile(path.join(__dirname, "views", "carrito.html")))
+
+app.get("/registro", (req, res) => res.sendFile(path.join(__dirname, "views", "registro.html")))
+
+app.get("/detalles-del-producto", (req, res) => res.sendFile(path.join(__dirname, "views", "detalles-del-producto.html")))
 
 app.listen(port, () => console.log("estoy funcionando en el puerto " + port));
