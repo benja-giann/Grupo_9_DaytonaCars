83b3896f734bc57db7691bf2a5c083b476696eb1
benja-giann <benjamingianneschi5@gmail.com>
2022-04-17 19:21:35 -0300

    home

--- src/app.js
+++ src/app.js
@@ -8,9 +8,9 @@ app.use(express.static(path.join(__dirname,"../", "public")));
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")))
 
-app.get("/carrito", (req, res) => res.sendFile(path.join(__dirname, "views", "carrito.html")))
+app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "carrito.html")))
 
-app.get("/registro", (req, res) => res.sendFile(path.join(__dirname, "views", "registro.html")))
+app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "views", "register.html")))
 
 app.get("/detalles-del-producto", (req, res) => res.sendFile(path.join(__dirname, "views", "detalles-del-producto.html")))
 
