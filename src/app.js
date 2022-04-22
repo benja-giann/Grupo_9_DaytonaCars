904e01269c13237380520f875d738f4ba3a9a592
benja-giann <benjamingianneschi5@gmail.com>
2022-04-22 12:21:58 -0300

    cambios en las vistas de html

--- src/app.js
+++ src/app.js
@@ -6,11 +6,13 @@ const port = 5500;
 app.use(express.static(path.join(__dirname,"../", "public")));
 
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")))
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")));
 
-app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "carrito.html")))
+app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")));
 
-app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "views", "register.html")))
+app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "views", "register.html")));
+
+app.get("/productos", (req, res) => res.sendFile(path.join(__dirname, "views", "productos.html")))
 
 app.get("/detalles-del-producto", (req, res) => res.sendFile(path.join(__dirname, "views", "detalles-del-producto.html")))
 
